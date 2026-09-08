bdb2998b42f392c38854e6108725b5f16ec0ecc4
lemon <bensalah.ibtihal2005@gmail.com>
2026-09-08 22:39:57 +0100

    added a basic version of the add_list and add_task commands

--- main.py
+++ main.py
@@ -12,7 +12,9 @@ intents = discord.Intents.default()
 intents.message_content = True
 intents.members = True
 
-bot = commands.Bot(command_prefix='!', intents=intents)
+bot = commands.Bot(command_prefix='/', intents=intents)
+
+list = {}
 
 
 @bot.event
@@ -35,5 +37,20 @@ async def on_message(message):
 @bot.command()
 async def ping(ctx):
     await ctx.send('Pong!')
+
+@bot.command()
+async def add_task(ctx, list_name: str, duration: int, *, task: str):
+    if list_name not in list:
+        await ctx.send(f'List "{list_name}" does not exist! Please create it first using !add_list command.')
+        return
     
+    list[list_name].append((task, duration))
+    await ctx.send(f'Task "{task}" is added to list "{list_name}"! You have {duration} days to finish it.')
+
+
+@bot.command()
+async def add_list(ctx, *, list_name: str):
+    list[list_name] = []
+    await ctx.send(f'"{list_name}" is added!')
+
 bot.run(TOKEN, log_handler=handler, log_level=logging.DEBUG)
